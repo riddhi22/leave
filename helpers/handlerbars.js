107a02ff19f9b1fbecb4d9a0bea6a904a4c8af0e
Siddharth Goyal <siddharth.game@gmail.com>
2017-10-28 17:51:10 +0530

    handlebars

---
+++ helpers/handlerbars.js
@@ -0,0 +1,44 @@
+var register = function(Handlebars) {
+    var helpers = {
+    inc: function(value, options) {
+        return parseInt(value) + 1;
+    },
+    foo: function(var1, var2) {
+        return ....
+    },
+    allequal: function(lvalue, rvalue, options) {
+    if (arguments.length < 3)
+        throw new Error("Handlebars Helper equal needs 2 parameters");
+    if( lvalue!=rvalue ) {
+        return options.inverse(this);
+    } else {
+        return options.fn(this);
+    }
+
+    },
+};
+
+if (Handlebars && typeof Handlebars.registerHelper === "function") {
+    for (var prop in helpers) {
+        Handlebars.registerHelper(prop, helpers[prop]);
+    }
+} else {
+    return helpers;
+}
+
+};
+
+module.exports.register = register;
+module.exports.helpers = register(null);
+
+/*
+exphbs.registerHelper('allequal', function(lvalue, rvalue, options) {
+    if (arguments.length < 3)
+        throw new Error("Handlebars Helper equal needs 2 parameters");
+    if( lvalue!=rvalue ) {
+        return options.inverse(this);
+    } else {
+        return options.fn(this);
+    }
+});
+*/
